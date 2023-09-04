680494278973d23842eb0f21edc6d12185a4046e
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-09-04 14:28:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,59 @@ const apiUrls = [
 ];
 
 // You can write your code here
+
+// Function to fetch data from a single API
+function fetchData(apiUrl) {
+  return fetch(apiUrl)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error(`Error fetching data from ${apiUrl}: ${error}`);
+      return null;
+    });
+}
+
+// Function to measure the time taken for a promise to resolve
+function measureTime(promise) {
+  const startTime = performance.now();
+  return promise
+    .then(() => performance.now() - startTime)
+    .catch((error) => {
+      console.error(`Error: ${error}`);
+      return null;
+    });
+}
+
+// Use Promise.all to fetch data from all APIs and measure the time taken
+const promiseAllStartTime = performance.now();
+Promise.all(apiUrls.map(fetchData))
+  .then(() => {
+    const promiseAllTime = performance.now() - promiseAllStartTime;
+    document.getElementById("output-all").textContent = promiseAllTime.toFixed(2) + " ms";
+  })
+  .catch((error) => console.error(`Promise.all Error: ${error}`));
+
+// Use Promise.any to fetch data from any API and measure the time taken
+const promiseAnyStartTime = performance.now();
+Promise.any(apiUrls.map(fetchData))
+  .then(() => {
+    const promiseAnyTime = performance.now() - promiseAnyStartTime;
+    document.getElementById("output-any").textContent = promiseAnyTime.toFixed(2) + " ms";
+  })
+  .catch((error) => console.error(`Promise.any Error: ${error}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
